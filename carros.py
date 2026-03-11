e32ed589d82a25996845617526b7cece7fa8680c
Ranilton <rasrj@cesar.school>
2026-03-11 11:51:53 -0300

    Buscar carros e remover carros

--- carros.py
+++ carros.py
@@ -20,3 +20,47 @@ def cadastrar_carro():
 
     print("Seu carro foi cadastrado com êxito!")
 
+
+def listar_carros():
+
+    if len(carros) == 0:
+        print("Não tem carros cadastrados!")
+        return
+    
+    for carro in carros:
+        print(f"{carro['marca']} - {carro['modelo']} - {carro['ano']}")
+
+
+def buscar_carro():
+
+    busca = input("Digite a marca do carro: ")
+
+    for carro in carros:
+
+        if carro["marca"].lower() == busca.lower():
+
+            print("Encontramos o carro:")
+            print(f"{carro['marca']} - {carro['modelo']} - {carro['ano']}")
+            return
+
+    print("O carro não foi encontrado!")
+
+
+def remover_carro():
+
+    modelo = input("Digite o modelo do carro para remover: ")
+
+    for carro in carros:
+
+        if carro["modelo"].lower() == modelo.lower():
+
+            carros.remove(carro)
+
+            salvar_carros(carros)
+
+            print("Carro removido com êxito!")
+            return
+
+    print("O carro não foi encontrado!")
+
+
