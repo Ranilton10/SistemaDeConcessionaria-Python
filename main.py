da5fdff10349da9264a47cf723ba33a7dbe9ee97
Ranilton <rasrj@cesar.school>
2026-03-11 15:52:11 -0300

    Atualização da main diante das novas funções

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ def menu():
 
     while True:
 
-        print("\n=== Sistema da Concessionária ===")
+        print("\n=== SISTEMA DA CONCESSIONÁRIA ===")
+
         print("1 - Cadastrar carro")
         print("2 - Listar carros")
         print("3 - Buscar carro")
         print("4 - Remover carro")
-        print("5 - Ordenar carros")
-        print("6 - Estatísticas da Concessionária")
+        print("5 - Ordenar carros por marca")
+        print("6 - Ordenar carros por preço")
+        print("7 - Estatísticas da concessionária")
+        print("8 - Relatório da concessionária")
         print("0 - Sair")
-
 
         opcao = input("Escolha uma opção: ")
 
@@ -29,13 +31,19 @@ def menu():
             remover_carro()
 
         elif opcao == "5":
-            ordenar_carros()
+            ordenar_por_marca()
 
         elif opcao == "6":
+            ordenar_por_preco()
+
+        elif opcao == "7":
             estatisticas()
 
+        elif opcao == "8":
+            relatorio()
+
         elif opcao == "0":
-            print("Ok... encerrando o sistema da concessionária!")
+            print("Entendido... encerrando o sistema!")
             break
 
         else:
